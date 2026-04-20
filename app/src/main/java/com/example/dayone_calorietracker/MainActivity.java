528ec8c3497804d860161db22cb549234fb5cee5
Majd Maatouk <maatoukmajd.520@gmail.com>
2026-04-20 20:04:37 +0300

    ...

--- app/src/main/java/com/example/dayone_calorietracker/MainActivity.java
+++ app/src/main/java/com/example/dayone_calorietracker/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.dayone_calorietracker;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +10,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.SHOW_PROGRESS);
+        progressBar.setMax(2000);
+        progressBar.setProgress(1000);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
